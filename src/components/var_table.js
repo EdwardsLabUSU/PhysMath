9dd29ef59d414846bd4941dc820229e7cff2f8ff
A02366638 <a02366638@aggies.usu.edu>
2023-05-08 15:21:05 -0600

    commiting

--- src/components/var_table.js
+++ src/components/var_table.js
@@ -17,6 +17,24 @@ export default function VarTable({data, setVals, arr, reDraw}){
     
     const DisplayData=data.map(
         (info, index)=>{
+
+          const handleInputChange = (e) => {
+            let newArr = arr;
+                                //for (let i = 0; i < arr.length; i++){
+                                //     if (e.target.id === arr[i].split(":")[0]){
+                                        if (e.target.value === 'true' || e.target.value === 'false'){
+                                            newArr[index] = e.target.checked
+                                        } else{
+                                            newArr[index] = Number(e.target.value)                     
+                                        }
+                                        console.log(index)
+                                //     }
+                                // }
+                                //newArr[index] = e.target.value
+                                // setVals.current=newArr
+                                setVals(arr => [...newArr])
+                                // console.log(e)
+          }
             return(
                 <tr>
                     <td className = 'varCol' data-toggle="tooltip" data-placement="right" title={info.tooltip}>{info.display} {info.name}</td>
@@ -29,25 +47,7 @@ export default function VarTable({data, setVals, arr, reDraw}){
                         <Slider
                         value = {arr[index]}
                           onBlur={reDraw}
-                          onChange={
-                            (e) => {
-                                let newArr = arr;
-                                //for (let i = 0; i < arr.length; i++){
-                                //     if (e.target.id === arr[i].split(":")[0]){
-                                        if (e.target.value === 'true' || e.target.value === 'false'){
-                                            newArr[index] = e.target.checked
-                                        } else{
-                                            newArr[index] = Number(e.target.value)                     
-                                        }
-                                        console.log(index)
-                                //     }
-                                // }
-                                //newArr[index] = e.target.value
-                                // setVals.current=newArr
-                                setVals(arr => [...newArr])
-                                console.log(e)
-                            }
-                          }
+                          onChange={handleInputChange}
                           aria-labelledby="input-slider"
                         />
                       </Grid>
@@ -56,24 +56,7 @@ export default function VarTable({data, setVals, arr, reDraw}){
                           value={arr[index]}
                           size="small"
                           onBlur={reDraw}
-                          onChange={
-                            (e) => {
-                                let newArr = arr;
-                                //for (let i = 0; i < arr.length; i++){
-                                //     if (e.target.id === arr[i].split(":")[0]){
-                                        if (e.target.value === 'true' || e.target.value === 'false'){
-                                            newArr[index] = e.target.checked
-                                        } else{
-                                            newArr[index] = Number(e.target.value)                     
-                                        }
-                                        console.log(index)
-                                //     }
-                                // }
-                                //newArr[index] = e.target.value
-                                // setVals.current=newArr
-                                setVals(arr => [...newArr])
-                            }
-                          }
+                          onChange={handleInputChange}
                           inputProps={{
                             step: 1,
                             min: info.min,
